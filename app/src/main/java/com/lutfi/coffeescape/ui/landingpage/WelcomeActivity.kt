a62a2e0868d0192b9fff29a00a20e752ac53b7a5
LutfiNurRohmah <lutfi0036ft.2021@student.uny.ac.id>
2023-12-10 22:56:13 +0700

    features: create WelcomeActivity

---
+++ app/src/main/java/com/lutfi/coffeescape/ui/landingpage/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.lutfi.coffeescape.ui.landingpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lutfi.coffeescape.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
