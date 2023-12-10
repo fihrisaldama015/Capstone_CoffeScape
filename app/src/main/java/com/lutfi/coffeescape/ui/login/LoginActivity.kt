5373035e9bb99e49dadb65ecfd66de542ed78cd0
LutfiNurRohmah <lutfi0036ft.2021@student.uny.ac.id>
2023-12-10 23:15:42 +0700

    features: create login layout

---
+++ app/src/main/java/com/lutfi/coffeescape/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.lutfi.coffeescape.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lutfi.coffeescape.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
