9cb506dc4046ba5e8d85abeb27352343a1327d69
LutfiNurRohmah <lutfi0036ft.2021@student.uny.ac.id>
2023-12-17 13:23:36 +0700

    feat: splash and welcome activity

--- app/src/main/java/com/lutfi/coffeescape/ui/landingpage/WelcomeActivity.kt
+++ app/src/main/java/com/lutfi/coffeescape/ui/landingpage/WelcomeActivity.kt
@@ -1,12 +1,32 @@
 package com.lutfi.coffeescape.ui.landingpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lutfi.coffeescape.R
+import com.lutfi.coffeescape.databinding.ActivityWelcomeBinding
+import com.lutfi.coffeescape.ui.login.LoginActivity
+import com.lutfi.coffeescape.ui.register.RegisterActivity
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.createAccountButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
