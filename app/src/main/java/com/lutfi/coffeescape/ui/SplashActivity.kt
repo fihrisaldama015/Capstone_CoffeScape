7aaac5875d9a5a2672e15fe0bf00dd4ed5deaef7
LutfiNurRohmah <lutfi0036ft.2021@student.uny.ac.id>
2023-12-17 13:23:27 +0700

    feat: splash and welcome activity

--- app/src/main/java/com/lutfi/coffeescape/ui/SplashActivity.kt
+++ app/src/main/java/com/lutfi/coffeescape/ui/SplashActivity.kt
@@ -1,12 +1,21 @@
 package com.lutfi.coffeescape.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.lutfi.coffeescape.R
+import com.lutfi.coffeescape.ui.landingpage.WelcomeActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 }
