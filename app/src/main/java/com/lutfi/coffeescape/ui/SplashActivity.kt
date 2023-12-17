7672813513093889d52291f0ab20d6ef4e8b5f6c
LutfiNurRohmah <lutfi0036ft.2021@student.uny.ac.id>
2023-12-17 14:54:46 +0700

    feat: add login activity

--- app/src/main/java/com/lutfi/coffeescape/ui/SplashActivity.kt
+++ app/src/main/java/com/lutfi/coffeescape/ui/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.lutfi.coffeescape.R
+import com.lutfi.coffeescape.ui.home.HomeActivity
 import com.lutfi.coffeescape.ui.landingpage.WelcomeActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -13,7 +14,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            val intent = Intent(this@SplashActivity, WelcomeActivity::class.java)
+            val intent = Intent(this@SplashActivity, HomeActivity::class.java)
             startActivity(intent)
             finish()
         }, 1000)
