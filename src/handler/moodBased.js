8ad0ae54ae9697514391ae64e51c3a0bf2ea731b
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-12-20 01:14:02 +0700

    [MOD] Mood Based

--- src/handler/moodBased.js
+++ src/handler/moodBased.js
@@ -1,35 +1,82 @@
-const readline = require('node:readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
+const { db } = require("../lib/firebase");
+
+const getHappyDrinks = async () => {
+  const response = await db
+    .collection("recipes")
+    .where("moodType", "==", "Happy")
+    .get();
+
+  let drinks = [];
+  response.forEach((doc) => {
+    drinks.push(doc.data());
   });
-let happyDrinks = [];
-let sadDrinks = [];
-let lonelyDrinks = [];
-let boredDrinks = [];
 
- function suggestDrink(mood) {
-    let drinks;
-    switch (mood.toLowerCase()) {
-        case "happy":
-            drinks = happyDrinks;
-            break;
-        case "sad":
-            drinks = sadDrinks;
-            break;
-        case "lonely":
-            drinks = lonelyDrinks;
-            break;
-        case "bored":
-            drinks = boredDrinks;
-            break;
-        default:
-            return "Invalid mood. Please choose from: happy, sad, lonely, or bored.";
-    }
+  return drinks;
+};
 
-    let selectedDrink = drinks[Math.floor(Math.random() * drinks.length)];
-    return `For your ${mood} mood, I suggest a ${selectedDrink}.`;
+const getSadDrinks = async () => {
+  const response = await db
+    .collection("recipes")
+    .where("moodType", "==", "Sad")
+    .get();
+  let drinks = [];
+  response.forEach((doc) => {
+    drinks.push(doc.data());
+  });
+
+  return drinks;
+};
+
+const getLonelyDrinks = async () => {
+  const response = await db
+    .collection("recipes")
+    .where("moodType", "==", "Lonely")
+    .get();
+  let drinks = [];
+  response.forEach((doc) => {
+    drinks.push(doc.data());
+  });
+  return drinks;
+};
+
+const getBoredDrinks = async () => {
+  const response = await db
+    .collection("recipes")
+    .where("moodType", "==", "Bored")
+    .get();
+
+  let drinks = [];
+  response.forEach((doc) => {
+    drinks.push(doc.data());
+  });
+
+  return drinks;
+};
+
+async function suggestDrink(mood) {
+  let drinks;
+  switch (mood.toLowerCase()) {
+    case "happy":
+      drinks = await getHappyDrinks();
+      break;
+    case "sad":
+      drinks = await getSadDrinks();
+      break;
+    case "lonely":
+      drinks = await getLonelyDrinks();
+      break;
+    case "bored":
+      drinks = await getBoredDrinks();
+      break;
+    default:
+      return "Invalid mood. Please choose from: happy, sad, lonely, or bored.";
+  }
+
+  let selectedDrink = drinks[Math.floor(Math.random() * drinks.length)];
+  return {
+    message: `For your ${mood} mood, I suggest a ${selectedDrink.name}.`,
+    data: selectedDrink,
+  };
 }
-
-
 
 module.exports = { suggestDrink };
