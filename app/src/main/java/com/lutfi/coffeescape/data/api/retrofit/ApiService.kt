25568723ea41cb4f9a3590d54d5ff6b20705120a
LutfiNurRohmah <lutfi0036ft.2021@student.uny.ac.id>
2023-12-21 09:58:06 +0700

    feat: update apiservice

--- app/src/main/java/com/lutfi/coffeescape/data/api/retrofit/ApiService.kt
+++ app/src/main/java/com/lutfi/coffeescape/data/api/retrofit/ApiService.kt
@@ -1,14 +1,14 @@
 package com.lutfi.coffeescape.data.api.retrofit
 
+import com.lutfi.coffeescape.data.api.response.AddRatingResponse
 import com.lutfi.coffeescape.data.api.response.CoffeeResponse
-import com.lutfi.coffeescape.data.api.response.DataCoffee
 import com.lutfi.coffeescape.data.api.response.DeleteFavoriteRequest
 import com.lutfi.coffeescape.data.api.response.DetailCoffeeResponse
 import com.lutfi.coffeescape.data.api.response.LoginResponse
+import com.lutfi.coffeescape.data.api.response.RecommendationResponse
 import com.lutfi.coffeescape.data.api.response.RegisterResponse
 import com.lutfi.coffeescape.data.api.response.UserProfileResponse
 import retrofit2.http.Body
-import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
@@ -55,8 +55,25 @@ interface ApiService {
         @Body requestBody: DeleteFavoriteRequest
     ): UserProfileResponse
 
-    @GET("arabica/{id}")
+    @GET("coffee/{id}")
     suspend fun getCoffeeById(
         @Path("id") id: String
     ): DetailCoffeeResponse
+
+    @FormUrlEncoded
+    @POST("users/{id}/rating")
+    suspend fun AddCoffeeRating(
+        @Path("id") id: String,
+        @Field("coffeeId") coffeeId: String,
+        @Field("rating") rating: String,
+        @Field("comment") comment: String
+    ): AddRatingResponse
+
+    @GET("coffee")
+    suspend fun getAllCoffee(): CoffeeResponse
+
+    @GET("recommendation/{id}")
+    suspend fun getRecommendationCoffee(
+        @Path("id") id : String
+    ): RecommendationResponse
 }
